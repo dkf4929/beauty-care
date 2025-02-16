3d971f128c76fe50c5050c6643fbe67da744bedc
dkf4929 <sng4929@naver.com>
2025-02-16 16:18:34 +0900

    first commit

---
+++ src/test/java/com/project/beauty_care/BeautyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.beauty_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
