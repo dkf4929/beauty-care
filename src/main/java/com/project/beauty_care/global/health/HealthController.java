bc49cb09a8b053af57884d90a129fde0150b693a
dkf4929 <sng4929@naver.com>
2025-02-18 12:43:31 +0900

    chore: health check

---
+++ src/main/java/com/project/beauty_care/global/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.project.beauty_care.global.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck(){}
+}
