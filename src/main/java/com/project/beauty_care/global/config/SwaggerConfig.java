5d647db58e608dc06cd64bd7be02b9a7ec1fcbcb
dkf4929 <sng4929@naver.com>
2025-02-19 15:37:37 +0900

    B fix: api 호출 오류로 인한 주석 처리

--- src/main/java/com/project/beauty_care/global/config/SwaggerConfig.java
+++ src/main/java/com/project/beauty_care/global/config/SwaggerConfig.java
@@ -1,39 +1,39 @@
 package com.project.beauty_care.global.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@SecurityScheme(
-        name = "Authorization",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "Bearer"
-)
-@Configuration
+//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+//import io.swagger.v3.oas.annotations.security.SecurityScheme;
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@SecurityScheme(
+//        name = "Authorization",
+//        type = SecuritySchemeType.HTTP,
+//        bearerFormat = "JWT",
+//        scheme = "Bearer"
+//)
+//@Configuration
 public class SwaggerConfig {
     private static final String TITLE = "BEAUTY CARE";
     private static final String DESCRIPTION = "BEAUTY CARE REST API DOCUMENTATION";
     private static final String VERSION = "1.0.0";
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title(TITLE)
-                .description(DESCRIPTION)
-                .version(VERSION);
-    }
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI()
+//                .components(new Components())
+//                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+//                .info(apiInfo());
+//    }
+//
+//    private Info apiInfo() {
+//        return new Info()
+//                .title(TITLE)
+//                .description(DESCRIPTION)
+//                .version(VERSION);
+//    }
 }
 
--- src/main/java/com/project/beauty_care/global/health/HealthController.java
+++ src/main/java/com/project/beauty_care/global/health/HealthController.java
@@ -1,10 +1,10 @@
 package com.project.beauty_care.global.health;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.media.Content;
+//import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,13 +15,13 @@ public class HealthController {
     // server health check
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "헬스 체크",
-            description = "서버 상태 health 여부 확인")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "healthy"
-            )
-    })
+//    @Operation(summary = "헬스 체크",
+//            description = "서버 상태 health 여부 확인")
+//    @ApiResponses(value = {
+//            @ApiResponse(
+//                    responseCode = "200",
+//                    description = "healthy"
+//            )
+//    })
     public void healthCheck(){}
 }
