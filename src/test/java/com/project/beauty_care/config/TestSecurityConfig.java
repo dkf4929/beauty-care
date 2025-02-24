31705417452f71dc92550b160855ebeaa93cca35
dkf4929 <sng4929@naver.com>
2025-02-24 17:17:25 +0900

    chore: 불필요한 코드 제거

--- src/test/java/com/project/beauty_care/config/TestSecurityConfig.java
+++ src/test/java/com/project/beauty_care/config/TestSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.project.beauty_care.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 // 컨트롤러 테스트용
 @TestConfiguration
-@RequiredArgsConstructor
 public class TestSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
