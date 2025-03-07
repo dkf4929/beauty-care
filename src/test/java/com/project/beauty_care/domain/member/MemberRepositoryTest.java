de00c4c9f8076fd6993d71636ccbdb4e79eb5a91
dkf4929 <sng4929@naver.com>
2025-03-07 13:05:48 +0900

    test: memberRepository

--- src/test/java/com/project/beauty_care/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/project/beauty_care/domain/member/MemberRepositoryTest.java
@@ -7,11 +7,9 @@ import com.project.beauty_care.global.exception.RequestInvalidException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Transactional
 class MemberRepositoryTest extends RepositoryTestSupport {
     @Autowired
     private MemberRepository repository;
