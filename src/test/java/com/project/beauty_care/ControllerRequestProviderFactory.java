4a9cdd1cece895272a78c24812745dbc67aada34
dkf4929 <sng4929@naver.com>
2025-03-07 17:11:55 +0900

    refactor: 컨트롤러 Request factory

---
+++ src/test/java/com/project/beauty_care/ControllerRequestProviderFactory.java
@@ -0,0 +1,47 @@
+package com.project.beauty_care;
+
+import com.project.beauty_care.domain.member.dto.MemberCreateRequest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class ControllerRequestProviderFactory {
+    // MemberController
+    public static Stream<Arguments> validProvider() {
+        return Stream.of(
+                Arguments.of(new MemberCreateRequest("admin", "qwer1234", "admin")),
+                Arguments.of(new MemberCreateRequest("user", "qwer12345", "user")),
+                Arguments.of(new MemberCreateRequest("user1", "qwer123456", "user1"))
+        );
+    }
+
+    public static Stream<Arguments> invalidPasswordPatternProvider() {
+        return Stream.of(
+                Arguments.of(new MemberCreateRequest("admin", "12345", "admin")),
+                Arguments.of(new MemberCreateRequest("admin", "12345678", "admin")),
+                Arguments.of(new MemberCreateRequest("admin", "12345678123456789", "admin")),
+                Arguments.of(new MemberCreateRequest("admin", "aa", "admin")),
+                Arguments.of(new MemberCreateRequest("admin", "aaaaaaaa", "admin"))
+        );
+    }
+
+    public static Stream<Arguments> emptyFieldProvider() {
+        return Stream.of(
+                Arguments.of(new MemberCreateRequest("", "", ""))
+        );
+    }
+
+    public static Stream<Arguments> invalidLoginIdProvider() {
+        return Stream.of(
+                Arguments.of(new MemberCreateRequest("dd", "qwer1234", "user")),
+                Arguments.of(new MemberCreateRequest("ddddddddddd", "qwer1234", "user"))
+        );
+    }
+
+    public static Stream<Arguments> invalidNameProvider() {
+        return Stream.of(
+                Arguments.of(new MemberCreateRequest("user", "qwer1234", "d")),
+                Arguments.of(new MemberCreateRequest("user", "qwer1234", "ddddddddddddddddddddd"))
+        );
+    }
+}
--- src/test/java/com/project/beauty_care/domain/member/MemberControllerTest.java
+++ src/test/java/com/project/beauty_care/domain/member/MemberControllerTest.java
@@ -6,12 +6,9 @@ import com.project.beauty_care.global.enums.ErrorCodes;
 import com.project.beauty_care.global.enums.SuccessResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MemberControllerTest extends ControllerTestSupport {
     @DisplayName("사용자 정보를 입력해서, 회원가입 한다.")
     @ParameterizedTest
-    @MethodSource("validProvider")
+    @MethodSource("com.project.beauty_care.ControllerRequestProviderFactory#validProvider")
     void createMember(MemberCreateRequest request) throws Exception {
         // given
         when(memberService.createMember(any(MemberCreateRequest.class))).thenReturn(new Member());
@@ -37,7 +34,7 @@ class MemberControllerTest extends ControllerTestSupport {
 
     @DisplayName("사용자 정보를 입력하지 않고, 회원가입 시도하면, 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("emptyFieldProvider")
+    @MethodSource("com.project.beauty_care.ControllerRequestProviderFactory#emptyFieldProvider")
     void createMemberWithBlankRequest(MemberCreateRequest request) throws Exception {
         // when, then
         performPost("/member", request)
@@ -51,7 +48,7 @@ class MemberControllerTest extends ControllerTestSupport {
 
     @DisplayName("잘못된 비밀번호 패턴으로 회원가입 시도하면, 예외가 발생한다.")
     @ParameterizedTest
-    @MethodSource("invalidPasswordPatternProvider")
+    @MethodSource("com.project.beauty_care.ControllerRequestProviderFactory#invalidPasswordPatternProvider")
     void createMemberWithInvalidPassword(MemberCreateRequest request) throws Exception {
         // when, then
         performPost("/member", request)
@@ -62,7 +59,7 @@ class MemberControllerTest extends ControllerTestSupport {
 
     @DisplayName("로그인 ID 4~10 자리의 문자")
     @ParameterizedTest
-    @MethodSource("invalidLoginIdProvider")
+    @MethodSource("com.project.beauty_care.ControllerRequestProviderFactory#invalidLoginIdProvider")
     void createMemberWithInvalidLoginId(MemberCreateRequest request) throws Exception {
         // when, then
         performPost("/member", request)
@@ -73,7 +70,7 @@ class MemberControllerTest extends ControllerTestSupport {
 
     @DisplayName("로그인 ID 4~10 자리의 문자")
     @ParameterizedTest
-    @MethodSource("invalidNameProvider")
+    @MethodSource("com.project.beauty_care.ControllerRequestProviderFactory#invalidNameProvider")
     void createMemberWithInvalidName(MemberCreateRequest request) throws Exception {
         // when, then
         performPost("/member", request)
@@ -86,47 +83,5 @@ class MemberControllerTest extends ControllerTestSupport {
         return mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)));
-    }
-
-    private static Stream<Arguments> validProvider() {
-        return Stream.of(
-                Arguments.of(new MemberCreateRequest("admin", "qwer1234", "admin")),
-                Arguments.of(new MemberCreateRequest("user", "qwer12345", "user")),
-                Arguments.of(new MemberCreateRequest("user1", "qwer123456", "user1"))
-        );
-    }
-
-    private static Stream<Arguments> invalidPasswordPatternProvider() {
-        return Stream.of(
-                // 최소 8글자
-                Arguments.of(new MemberCreateRequest("admin", "12345", "admin")),
-                // 영문 + 숫자 조합
-                Arguments.of(new MemberCreateRequest("admin", "12345678", "admin")),
-                // 최대 16글자
-                Arguments.of(new MemberCreateRequest("admin", "12345678123456789", "admin"))
-        );
-    }
-
-
-    private static Stream<Arguments> emptyFieldProvider() {
-        return Stream.of(
-                Arguments.of(new MemberCreateRequest("", "", ""))
-        );
-    }
-
-    private static Stream<Arguments> invalidLoginIdProvider() {
-        return Stream.of(
-                // ID 최소 4
-                Arguments.of(new MemberCreateRequest("dd", "qwer1234", "user")),
-                // ID 최대 10
-                Arguments.of(new MemberCreateRequest("ddddddddddd", "qwer1234", "user"))
-        );
-    }
-
-    private static Stream<Arguments> invalidNameProvider() {
-        return Stream.of(
-                Arguments.of(new MemberCreateRequest("user", "qwer1234", "d")),
-                Arguments.of(new MemberCreateRequest("user", "qwer1234", "ddddddddddddddddddddd"))
-        );
     }
 }
