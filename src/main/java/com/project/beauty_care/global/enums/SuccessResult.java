00cbfb9d7157c19ac9573770cf5eda8f81710344
dkf4929 <sng4929@naver.com>
2025-03-12 16:08:55 +0900

    chore: 코드 추가

--- src/main/java/com/project/beauty_care/global/enums/SuccessResult.java
+++ src/main/java/com/project/beauty_care/global/enums/SuccessResult.java
@@ -10,6 +10,7 @@ public enum SuccessResult {
     LOGIN_SUCCESS("L001", "로그인 성공"),
     SAVE_SUCCESS("S001", "저장이 완료 되었습니다."),
     DELETE_SUCCESS("D001", "삭제 되었습니다."),
+    RETRIEVE_SUCCESS("R001", "조회가 완료 되었습니다."),
 
     // member
     MEMBER_SAVE_SUCCESS("M001", "회원가입이 완료 되었습니다.");
