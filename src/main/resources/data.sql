8dbfa4146fe765a25693513310b8efbfc4b99e7d
dkf4929 <sng4929@naver.com>
2025-03-24 16:45:58 +0900

    chore: 초기 데이터 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 insert into beauty_care.member
-    (id, login_id, name, password, role, created_date, updated_date, created_by, updated_by, last_login_date_time)
+    (id, login_id, name, password, role, created_date_time, updated_date_time, created_by, updated_by, last_login_date_time)
 values
     (1, 'admin', '관리자', '$2a$10$NBx89slgit4rozmzdjtqSONMsiTEjLNgeV2eJYS9agGaNO1fq.NSK',
      'ROLE_ADMIN', null, null, null, null, null);
+
+insert into beauty_care.code(`name`,`id`,`upper_id`,`description`,`sort_number`,`is_use`,`created_date_time`,`updated_date_time`)VALUES
+('시스템', 'sys', null, '시스템(최상위)', 1, true, NOW(), NOW()),
+('동의 상태', 'sys:agree', 'sys', '동의 상태', 1, true, NOW(), NOW()),
+('동의', 'sys:agree:Y', 'sys:agree', '동의', 1, true, NOW(), NOW()),
+('미동의', 'sys:agree:N', 'sys:agree', '미동의', 2, true, NOW(), NOW());
