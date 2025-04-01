612535c212a42c2ddb661a8f2ba8c9534dbd97e9
dkf4929 <sng4929@naver.com>
2025-04-01 16:39:09 +0900

    test: SecurityAuditorAware

---
+++ src/test/java/com/project/beauty_care/global/config/SpringSecurityAuditorAwareTest.java
@@ -0,0 +1,79 @@
+package com.project.beauty_care.global.config;
+
+import com.project.beauty_care.IntegrationTestSupport;
+import com.project.beauty_care.domain.role.Role;
+import com.project.beauty_care.global.enums.Authentication;
+import com.project.beauty_care.global.security.dto.AppUser;
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.testcontainers.shaded.com.google.common.collect.Maps;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringSecurityAuditorAwareTest extends IntegrationTestSupport {
+    @Autowired
+    private SpringSecurityAuditorAware springSecurityAuditorAware;
+
+    @BeforeEach
+    void setUp() {
+        SecurityContextHolder.clearContext();
+    }
+
+    @DisplayName("get authentication test")
+    @ParameterizedTest
+    @MethodSource("authenticationProvider")
+    void getCurrentAuditor(AppUser authentication, Role role, Long expectedMemberId) {
+        // given
+        setAuthentication(authentication, role);
+
+        // when
+        Long memberId = springSecurityAuditorAware.getCurrentAuditor().orElse(null);
+
+        // then
+        assertThat(memberId).isEqualTo(expectedMemberId);
+    }
+
+    private static void setAuthentication(AppUser authentication, Role role) {
+        if (ObjectUtils.isNotEmpty(authentication) && ObjectUtils.isNotEmpty(role)) {
+            SecurityContextHolder.getContext().setAuthentication(
+                    new UsernamePasswordAuthenticationToken(
+                            authentication,
+                            null,
+                            List.of(new SimpleGrantedAuthority(role.getRoleName()))
+                    )
+            );
+        } else
+            SecurityContextHolder.clearContext();
+    }
+
+    private static Stream<Arguments> authenticationProvider() {
+        Role adminRole = Role.builder()
+                .roleName(Authentication.ADMIN.getName())
+                .urlPatterns(Maps.newHashMap())
+                .isUse(Boolean.TRUE)
+                .build();
+
+        AppUser adminUser = AppUser.builder()
+                .memberId(1L)
+                .role(adminRole)
+                .name("admin")
+                .loginId("admin")
+                .build();
+
+        return Stream.of(
+                Arguments.of(adminUser, adminRole, 1L),
+                Arguments.of(null, null, null)
+        );
+    }
+}
