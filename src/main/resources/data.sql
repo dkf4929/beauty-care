7606d94b194e6c9eb06544f8812cfd51761ab381
dkf4929 <sng4929@naver.com>
2025-04-04 09:23:41 +0900

    chore: 일부 컬럼 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into beauty_care.member
-    (id, login_id, name, password, role, created_date_time, updated_date_time, created_by, updated_by, last_login_date_time)
+    (id, login_id, name, password, role_id, is_use, created_date_time, updated_date_time, created_by, updated_by, last_login_date_time)
 values
     (1, 'admin', '관리자', '$2a$10$NBx89slgit4rozmzdjtqSONMsiTEjLNgeV2eJYS9agGaNO1fq.NSK',
-     'ROLE_ADMIN', null, null, null, null, null);
+     'ADMIN', true,null, null, null, null, null);
 
 insert into beauty_care.code(`name`,`id`,`upper_id`,`description`,`sort_number`,`is_use`,`created_date_time`,`updated_date_time`)VALUES
 ('시스템', 'sys', null, '시스템(최상위)', 1, true, NOW(), NOW()),
