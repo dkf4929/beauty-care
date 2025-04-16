c003de4f4ac3e1a1a0d45f087dd0bfc8aa58c196
dkf4929 <sng4929@naver.com>
2025-04-16 14:31:05 +0900

    fix: customRepository mock bean

--- src/test/java/com/project/beauty_care/RepositoryTestSupport.java
+++ src/test/java/com/project/beauty_care/RepositoryTestSupport.java
@@ -1,9 +1,16 @@
 package com.project.beauty_care;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @Transactional
 public abstract class RepositoryTestSupport extends DataBaseConnectionSupport {
+    // 테스트 시 별도로 주입 필요.
+    @MockitoBean
+    private JPAQueryFactory queryFactory;
 }
