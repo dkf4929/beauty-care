30a4ca2294b3703d6a46b3909af2f751e4cc41a4
dkf4929 <sng4929@naver.com>
2025-05-07 09:26:25 +0900

    refactor: swaggerConfig

--- src/main/java/com/project/beauty_care/global/config/SwaggerConfig.java
+++ src/main/java/com/project/beauty_care/global/config/SwaggerConfig.java
@@ -9,20 +9,12 @@ import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springdoc.core.customizers.OperationCustomizer;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.method.HandlerMethod;
-
-import java.lang.reflect.Field;
-import java.util.Locale;
 
 @SecurityScheme(
         name = "Authorization",
@@ -36,7 +28,7 @@ public class SwaggerConfig {
     private static final String TITLE = "BEAUTY CARE";
     private static final String DESCRIPTION = "BEAUTY CARE REST API DOCUMENTATION";
     private static final String VERSION = "1.0.0";
-    private final MessageSource messageSource;
+    private final SwaggerResponseSchemaProvider provider;
 
     @Bean
     public OpenAPI openAPI() {
@@ -74,107 +66,9 @@ public class SwaggerConfig {
                 content.keySet()
                         .forEach(mediaTypeKey -> {
                             final MediaType mediaType = content.get(mediaTypeKey);
-                            mediaType.schema(wrapSchema(mediaType.getSchema(), type, wrapFieldName, handlerMethod));
+                            mediaType.schema(provider.wrapSchema(mediaType.getSchema(), type, wrapFieldName, handlerMethod));
                         });
             }
         }
-    }
-
-    // 공통 응답 wrapping 처리
-    @SneakyThrows
-    private <T> Schema<T> wrapSchema(Schema<?> originalSchema, Class<T> type, String wrapFieldName, HandlerMethod handlerMethod) {
-        final Schema<T> wrapperSchema = new Schema<>();
-
-        // httpMethod
-        RequestMethod requestMethod = handlerMethod.getMethodAnnotation(RequestMapping.class).method()[0];
-
-        String methodName = handlerMethod.getMethod().getName();
-
-        final String CODE = "successCode";
-        final String MESSAGE = "successMessage";
-
-        for (Field field : type.getDeclaredFields()) {
-            field.setAccessible(true);
-
-            switch (requestMethod) {
-                case GET:
-                    if (field.getName().equals(CODE)) {
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("200"));
-                    }
-
-                    if (field.getName().equals(MESSAGE)) {
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("조회가 완료 되었습니다."));
-                    }
-                    break;
-                case POST:
-                    if (methodName.contains("create")) {
-                        // 저장 - 201
-                        if (field.getName().equals(CODE))
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("201"));
-                        else if (field.getName().equals(MESSAGE)) {
-                            String entity = methodName.replaceAll("^[a-z]+", "");
-                            String localizedEntity = messageSource.getMessage(entity, null, Locale.KOREA);
-
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(localizedEntity + " 저장이 완료 되었습니다."));
-                        }
-                    } else if (methodName.equals("login")) {
-                        // 로그인 - 200
-                        if (field.getName().equals(CODE))
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("200"));
-                        else if (field.getName().equals(MESSAGE)) {
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("로그인 성공"));
-                        }
-                    } else if (methodName.contains("find")) {
-                        // 조회 - 200
-                        if (field.getName().equals(CODE))
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("200"));
-                        else if (field.getName().equals(MESSAGE)) {
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("조회가 완료 되었습니다."));
-                        }
-                    }
-                    else
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("작업이 완료 되었습니다."));
-
-                    break;
-                case DELETE:
-                    String message = "";
-
-                    if (methodName.equals("deleteMember")) message = "탈퇴 완료 되었습니다.";
-                    else message = "삭제 완료 되었습니다.";
-
-                    if (field.getName().equals(CODE))
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("204"));
-
-                    if (field.getName().equals(MESSAGE))
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example(message));
-
-                    break;
-                case PATCH:
-                    if (methodName.equals("deleteCancelMember")) {
-                        if (field.getName().equals(CODE))
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("204"));
-
-
-                        if (field.getName().equals(MESSAGE))
-                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("탈퇴 취소 되었습니다."));
-                    }
-
-                    break;
-                case PUT:
-                    if (field.getName().equals(CODE))
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("200"));
-
-
-                    if (field.getName().equals(MESSAGE))
-                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("수정 완료되었습니다."));
-                    break;
-                default:
-                    wrapperSchema.addProperty(field.getName(), new Schema<>());
-            }
-
-            field.setAccessible(false);
-        }
-        wrapperSchema.addProperty(wrapFieldName, originalSchema);
-        return wrapperSchema;
     }
 }
---
+++ src/main/java/com/project/beauty_care/global/config/SwaggerResponseSchemaProvider.java
@@ -0,0 +1,138 @@
+package com.project.beauty_care.global.config;
+
+import com.project.beauty_care.global.enums.SuccessCodes;
+import io.swagger.v3.oas.models.media.Schema;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+
+import java.lang.reflect.Field;
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class SwaggerResponseSchemaProvider {
+    private final MessageSource messageSource;
+
+    private <T> void constructWrapSchema(Class<T> type,
+                                         RequestMethod requestMethod,
+                                         Schema<T> wrapperSchema,
+                                         String methodName) {
+        final String CODE = "successCode";
+        final String MESSAGE = "successMessage";
+
+        for (Field field : type.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            switch (requestMethod) {
+                case GET:
+                    if (field.getName().equals(CODE)) {
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.RETRIEVE_SUCCESS.getCode()));
+                    }
+
+                    if (field.getName().equals(MESSAGE)) {
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.RETRIEVE_SUCCESS.getMessage()));
+                    }
+                    break;
+                case POST:
+                    if (methodName.contains("create")) {
+                        // 저장 - 201
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.SAVE_SUCCESS.getCode()));
+                        else if (field.getName().equals(MESSAGE)) {
+                            String entity = methodName.replaceAll("^[a-z]+", "");
+                            String localizedEntity = messageSource.getMessage(entity, null, Locale.KOREA);
+
+                            wrapperSchema.addProperty(field.getName(),
+                                    new Schema<>().example(localizedEntity + " " + SuccessCodes.SAVE_SUCCESS.getMessage()));
+                        }
+                    } else if (methodName.equals("login")) {
+                        // 로그인 - 200
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.LOGIN_SUCCESS.getCode()));
+                        else if (field.getName().equals(MESSAGE)) {
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.LOGIN_SUCCESS.getMessage()));
+                        }
+                    } else if (methodName.contains("find")) {
+                        // 조회 - 200
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.RETRIEVE_SUCCESS.getCode()));
+                        else if (field.getName().equals(MESSAGE)) {
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.RETRIEVE_SUCCESS.getMessage()));
+                        }
+                    } else if (methodName.equals("uploadTempFile") || methodName.equals("uploadFile")) {
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.FILE_UPLOAD_SUCCESS.getCode()));
+                        else if (field.getName().equals(MESSAGE)) {
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.FILE_UPLOAD_SUCCESS.getMessage()));
+                        }
+                    } else if (methodName.contains("scheduler")) {
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.SCHEDULER_EXECUTE_SUCCESS.getCode()));
+                        else if (field.getName().equals(MESSAGE)) {
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example(SuccessCodes.SCHEDULER_EXECUTE_SUCCESS.getMessage()));
+                        }
+                    }
+                    else
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("작업이 완료 되었습니다."));
+
+                    break;
+                case DELETE:
+                    String message = "";
+
+                    if (methodName.equals("deleteMember")) message = "탈퇴 완료 되었습니다.";
+                    else message = "삭제 완료 되었습니다.";
+
+                    if (field.getName().equals(CODE))
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("204"));
+
+                    if (field.getName().equals(MESSAGE))
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example(message));
+
+                    break;
+                case PATCH:
+                    if (methodName.equals("deleteCancelMember")) {
+                        if (field.getName().equals(CODE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("204"));
+
+
+                        if (field.getName().equals(MESSAGE))
+                            wrapperSchema.addProperty(field.getName(), new Schema<>().example("탈퇴 취소 되었습니다."));
+                    }
+
+                    break;
+                case PUT:
+                    if (field.getName().equals(CODE))
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("200"));
+
+
+                    if (field.getName().equals(MESSAGE))
+                        wrapperSchema.addProperty(field.getName(), new Schema<>().example("수정 완료되었습니다."));
+                    break;
+                default:
+                    wrapperSchema.addProperty(field.getName(), new Schema<>());
+            }
+
+            field.setAccessible(false);
+        }
+    }
+
+    // 공통 응답 wrapping 처리
+    @SneakyThrows
+    public  <T> Schema<T> wrapSchema(Schema<?> originalSchema, Class<T> type, String wrapFieldName, HandlerMethod handlerMethod) {
+        final Schema<T> wrapperSchema = new Schema<>();
+
+        // httpMethod
+        RequestMethod requestMethod = handlerMethod.getMethodAnnotation(RequestMapping.class).method()[0];
+
+        String methodName = handlerMethod.getMethod().getName();
+
+        constructWrapSchema(type, requestMethod, wrapperSchema, methodName);
+        wrapperSchema.addProperty(wrapFieldName, originalSchema);
+        return wrapperSchema;
+    }
+}
