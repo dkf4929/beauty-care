b731d1f4261a7a4cb520366cd39a5d3221c5b9e2
dkf4929 <sng4929@naver.com>
2025-05-09 11:09:07 +0900

    chore: add entity graph

--- src/main/java/com/project/beauty_care/domain/code/repository/CodeRepository.java
+++ src/main/java/com/project/beauty_care/domain/code/repository/CodeRepository.java
@@ -1,6 +1,7 @@
 package com.project.beauty_care.domain.code.repository;
 
 import com.project.beauty_care.domain.code.Code;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface CodeRepository extends JpaRepository<Code, String> {
     Optional<Code> findByParentIsNull();
+
+    @EntityGraph(attributePaths = {"children"})
+    Optional<Code> findById(String id);
 }
