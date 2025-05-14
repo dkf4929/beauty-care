ebdcf6e3c84a18b9aa89d99c81d1cd01bba094dc
dkf4929 <sng4929@naver.com>
2025-05-14 11:47:50 +0900

    chore: 불필요한 필드 삭제

--- src/test/java/com/project/beauty_care/RepositoryTestSupport.java
+++ src/test/java/com/project/beauty_care/RepositoryTestSupport.java
@@ -1,14 +1,12 @@
 package com.project.beauty_care;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.project.beauty_care.global.config.QueryDslConfig;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
+@Import(QueryDslConfig.class)
 @Transactional
 public abstract class RepositoryTestSupport extends DataBaseConnectionSupport {
-    // 테스트 시 별도로 주입 필요.
-    @MockitoBean
-    private JPAQueryFactory queryFactory;
 }
