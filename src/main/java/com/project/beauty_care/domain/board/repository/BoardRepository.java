19b69bfbd4152ecdc4a908d3edf8e34ec274f863
dkf4929 <sng4929@naver.com>
2025-05-15 14:25:01 +0900

    chore: 작성자 검색용 메서드

--- src/main/java/com/project/beauty_care/domain/board/repository/BoardRepository.java
+++ src/main/java/com/project/beauty_care/domain/board/repository/BoardRepository.java
@@ -1,10 +1,16 @@
 package com.project.beauty_care.domain.board.repository;
 
 import com.project.beauty_care.domain.board.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardCustomRepository {
-    Boolean existsBoardByCreatedByAndCreatedDateTimeBetween(Long createdMemberId, LocalDateTime createdDateTime, LocalDateTime now);
+    Boolean existsBoardByCreatedByAndCreatedDateTimeBetween(Long createdMemberId,
+                                                            LocalDateTime createdDateTime,
+                                                            LocalDateTime now);
+
+    Page<Board> findByCreatedByAndIsUse(Long createdBy, Boolean isUse, Pageable pageable);
 }
